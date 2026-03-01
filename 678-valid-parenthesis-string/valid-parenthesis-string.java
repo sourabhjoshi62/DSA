57aba7cf4c60d2307854b835a86c6bfd65b09644
Sourabh <153990793+sourabhjoshi62@users.noreply.github.com>
2026-03-01 10:48:57 +0530

    Time: 1 ms (31.68%) | Memory: 42.8 MB (69.33%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '('){
+                open.push(i);
+            }
+            else if(c == '*'){
+                star.push(i);
+            }
+            else { // ')'
+                if(!open.isEmpty()){
+                    open.pop();
+                }
+                else if(!star.isEmpty()){
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        // Match remaining '(' with '*' AFTER them
+        while(!open.isEmpty()){
+            if(star.isEmpty()) return false;
+            
+            if(open.peek() < star.peek()){
+                open.pop();
+                star.pop();
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
